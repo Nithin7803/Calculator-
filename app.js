d8f030680b4ee7ec8eb5efc2d7b499e43d1cbca6
Nithin Kurra <125260553+Nithin7803@users.noreply.github.com>
2024-05-15 10:00:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// let age = 12
+// let result = age>18 ?"valid voter":"invalid voter"
+
+// let number = 3
+// let result = number == 1 ?"telugu":number==2 ?"hindi":number ==3 ?"tamil":"english"
+// console.log(result)
+
+// let number = 1723
+// if (number==1){
+    // console.log("telugu")
+// }
+// else if(number==2){
+    // console.log("hindi")
+// }
+// else if(number==3){
+    // console.log("kannada")
+// }
+// else{
+    // console.log("english")
+// }
+
+function nithin(heart){
+    console.log("nithin❤️❤️❤️" + heart)
+}
+
+nithin("aarthi")
+
+
+
